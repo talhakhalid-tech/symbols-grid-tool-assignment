496fe5d036bd16872ec6b14b76e392c94be4bd43
talhakhalid-tech <talhakhalid0323@gmail.com>
2024-01-08 13:56:07 +0500

    upd init script

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-sed -i "s/{{X}}/$1/g" config.js
-sed -i "s/{{Y}}/$2/g" config.js
+powershell -Command "(Get-Content config.js) -replace '{{X}}', '$1' -replace '{{Y}}', '$2' | Set-Content config.js"
 
 echo "Initialization complete!"
